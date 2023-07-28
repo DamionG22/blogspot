3043b6d0f0c0bf3b4788227abe42851b666ed668
blue butter <damiongallarza@gmail.com>
2023-07-27 20:51:09 -0700

    installed node modules, updated server and index.js files.

---
+++ index.js
@@ -0,0 +1,16 @@
+// imports modules
+const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const Sequelize = require("sequelize");
+
+// Creates express app 
+const app = express();
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,7 @@
+// imports modules
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
+// Creates express app 
+const app = express();
